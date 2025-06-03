b2c4bde14f1a4e8f6f2422c1449b5c7e42253753
Don Juan Javier <townofdon@gmail.com>
2025-06-03 15:05:20 -0400

    fix force buildup; add fast obj add, camera move

--- core/ForceOverTime.gd
+++ core/ForceOverTime.gd
@@ -1,14 +1,24 @@
 extends RefCounted
 class_name ForceOverTime
 
+enum {
+	COLLISION,
+	AFFECTOR,
+	TRACTOR_BEAM,
+	# etc.
+}
+
 var _force := Vector2.ZERO
 var _duration := 0.0
 var _timeLeft := 0.0
+var _type := 0
+var _entity_collided_with: Node
 
-func _init(force: Vector2, duration: float) -> void:
+func _init(force: Vector2, duration: float, type: int, entity_collided_with = null) -> void:
 	_force = force
 	_duration = duration
 	_timeLeft = duration
+	_entity_collided_with = entity_collided_with
 
 func debug() -> void:
 	print("f=", _force, ",t=", _timeLeft)
@@ -26,3 +36,9 @@ func get_t() -> float:
 
 func is_completed() -> bool:
 	return _timeLeft <= 0
+
+func did_collide_with(node: Node) -> bool:
+	return _entity_collided_with && node == _entity_collided_with
+
+func is_type(type: int) -> bool:
+	return _type == type
--- gameplay.gd
+++ gameplay.gd
@@ -1,20 +1,47 @@
 extends Node2D
 class_name Gameplay
 
+@export var camera_move_speed := 100
+@export var camera_zoom_speed := Vector2(1, 1)
 @onready var red = preload("res://entities/RedBall.tscn")
 @onready var blue = preload("res://entities/BlueBall.tscn")
+@onready var camera = $Camera2D
 
-func _input(event):
-	if event is InputEventMouseButton && event.pressed:		
+func _process(delta: float) -> void:
+	var pos := get_viewport().get_mouse_position()
+	var mouse_down := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+	var shift_pressed := Input.is_action_pressed("shift")
+	if mouse_down:
 		var node: Entity
-		var direction: Vector2
-		if (event.shift_pressed):
+		if (shift_pressed):
 			node = blue.instantiate()
-			direction = Vector2.LEFT
 		else:
 			node = red.instantiate()
-			direction = Vector2.RIGHT
-		self.get_parent().add_child(node)
-		node.global_position = event.position
+		self.add_child(node)
+		var rect := get_viewport_rect()
+		var center := rect.get_center()
+		var direction:Vector2 = (center - pos).normalized()
+		node.global_position = pos
 		node.direction = direction
 		node.velocity = node.speed * node.direction
+
+	# move camera
+	var camera_direction := Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		camera_direction += Vector2.UP
+	if Input.is_action_pressed("down"):
+		camera_direction += Vector2.DOWN
+	if Input.is_action_pressed("left"):
+		camera_direction += Vector2.LEFT
+	if Input.is_action_pressed("right"):
+		camera_direction += Vector2.RIGHT
+	if Input.is_action_pressed("zoom_in"):
+		camera.zoom = camera.zoom + camera_zoom_speed * delta
+	elif Input.is_action_pressed("zoom_out"):
+		camera.zoom = camera.zoom - camera_zoom_speed * delta
+		camera.zoom.x = max(camera.zoom.x, 0.1)
+		camera.zoom.y = max(camera.zoom.y, 0.1)
+	camera.position = camera.position + camera_direction * camera_move_speed * delta / camera.zoom
+
+	var num_objects := get_child_count()
+	print(Engine.get_frames_per_second(), ",", get_child_count())
