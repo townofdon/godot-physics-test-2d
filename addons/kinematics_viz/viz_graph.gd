900ca1c596584fc9264d6f3e1eff645cb2ff0ef7
Don Juan Javier <townofdon@gmail.com>
2025-06-14 20:24:07 -0400

    add mouse position to viz graph

--- addons/kinematics_viz/viz_graph.gd
+++ addons/kinematics_viz/viz_graph.gd
@@ -2,7 +2,7 @@
 extends Control
 class_name VizGraph
 
-const PADDING := 50
+const PADDING := 60
 const CIRCLE_SIZE := 3
 
 var COLOR_BG = Color.html("#1D2229")
@@ -14,11 +14,30 @@ var value_min:float = 0.0
 var value_max:float = 1.0
 var t_min:float = 0.0
 var t_max:float = 0.0
-var domain_max:float = 1.0
+var domain:float = 1.0
+
+var mouse_pos:Vector2 = Vector2.ZERO
+var is_mouse_in_graph:bool = false
 
 func _init() -> void:
-	custom_minimum_size = Vector2(10, 500)
+	custom_minimum_size = Vector2(10, 400)
 	queue_redraw()
+
+func _process(delta: float) -> void:
+	# check mouse position
+	if Engine.is_editor_hint():
+		var rect := get_rect()
+		var pos := get_local_mouse_position()
+		var prev_mouse_pos := mouse_pos
+		mouse_pos.x = inverse_lerp(PADDING, rect.size.x - PADDING, pos.x)
+		mouse_pos.y = 1 - inverse_lerp(PADDING, rect.size.y - PADDING, pos.y)
+		is_mouse_in_graph = true
+		if mouse_pos.x <= 0 || mouse_pos.y <= 0 || mouse_pos.x >= 1 || mouse_pos.y >= 1:
+			mouse_pos = Vector2.ZERO
+			is_mouse_in_graph = false
+		var did_change:bool = !prev_mouse_pos.is_equal_approx(mouse_pos)
+		if did_change:
+			queue_redraw()
 
 func set_data(data: Dictionary) -> void:
 	var points:Array[Vector2] = data["points"]
@@ -26,35 +45,40 @@ func set_data(data: Dictionary) -> void:
 	var value_max:float = data["max"]
 	var t_min:float = data["t_min"]
 	var t_max:float = data["t_max"]
-	var domain_max:float = data["t_end"]
+	var domain:float = data["t_end"]
 	assert(points is Array[Vector2])
 	assert(value_min is float)
 	assert(value_max is float)
 	assert(t_min is float)
 	assert(t_max is float)
-	assert(domain_max is float)
+	assert(domain is float)
 	self.points = points
 	self.value_min = value_min
 	self.value_max = value_max
 	self.t_min = t_min
 	self.t_max = t_max
-	self.domain_max = domain_max
+	self.domain = max(domain, 0.5)
 	queue_redraw()
 
 func _draw() -> void:
 	var rect := get_rect()
 	var resolution:String = "%1.1f"
-	if domain_max <= 0.5 + Constants.EPSILON:
+	if domain <= 0.5 + Constants.EPSILON:
 		resolution = "%1.2f"
+	if is_mouse_in_graph:
+		var mouse_x := mouse_pos.x * domain
+		var mouse_y := lerp(value_min, value_max, mouse_pos.y)
+		plot_horizontal_line(mouse_y, "%1.2f" % mouse_y, Color.DIM_GRAY, Color.LIGHT_SLATE_GRAY)
+		plot_vertical_line(mouse_x, 0, "%1.2f" % mouse_x, Color.DIM_GRAY, Color.LIGHT_SLATE_GRAY, PADDING - 20)
 	plot_horizontal_line(value_min, "%1.1f" % value_min, Color.TRANSPARENT, Color.AQUAMARINE)
 	plot_horizontal_line(value_max, "%1.1f" % value_max, Color.TRANSPARENT, Color.AQUAMARINE)
+	plot_horizontal_line(0.25, "", COLOR_DIM)
+	plot_horizontal_line(0.5, "", COLOR_DIM)
+	plot_horizontal_line(0.75, "", COLOR_DIM)
 	if t_min > 0 && (resolution % t_min != "0.0"):
 		plot_vertical_line(t_min, 0, resolution % t_min, Color.DARK_MAGENTA, Color.DARK_ORCHID, PADDING - 20)
 	if t_max > 0:
 		plot_vertical_line(t_max, 0, resolution % t_max, Color.DARK_CYAN, Color.DARK_CYAN, PADDING - 20)
-	plot_horizontal_line(0.25, "", COLOR_DIM)
-	plot_horizontal_line(0.5, "", COLOR_DIM)
-	plot_horizontal_line(0.75, "", COLOR_DIM)
 	plot_horizontal_line(0, "0", Color.DARK_MAGENTA)
 	plot_horizontal_line(1, "1", Color.DARK_CYAN)
 	plot_vertical_lines()
@@ -68,7 +92,7 @@ func plot_horizontal_line(y_value:float, y_display: String, color: Color, text_c
 	var y = t * (rect.size.y - PADDING * 2) + PADDING
 	draw_line(Vector2(x0,y), Vector2(x1,y), color, 1, true)
 	if !len(y_display): return
-	var offset:float = min(len(y_display), 3) - 1
+	var offset:float = min(len(y_display), 4) - 1
 	var text_x:float = 0.0
 	draw_rect(Rect2(Vector2(x0 - 50, y - 20), Vector2(48, 32)), COLOR_BG)
 	draw_string(default_font, Vector2(x0 - 24 - 10 * offset,y + 6), y_display, HORIZONTAL_ALIGNMENT_LEFT, -1, 24, text_color)
@@ -76,7 +100,7 @@ func plot_horizontal_line(y_value:float, y_display: String, color: Color, text_c
 func plot_vertical_lines() -> void:
 	var t = 0.0
 	var min_x_next_label = 0.0
-	while t <= max(domain_max, 0.5):
+	while t <= domain:
 		var is_major: bool = floor(t) == t
 		var color := Color.DIM_GRAY
 		if !is_major:
@@ -92,7 +116,7 @@ func plot_vertical_line(time: float, min_x_next_label: float, label: String, col
 	var is_major: bool = floor(time) == time
 	const y0 = PADDING
 	var y1 := rect.size.y - PADDING
-	var t:float = time / max(domain_max, 0.5)
+	var t:float = time / domain
 	var x:float = t * (rect.size.x - PADDING * 2) + PADDING
 	draw_line(Vector2(x,y0), Vector2(x,y1), color, 1, true)
 	var offset := len(label) - 1
@@ -118,7 +142,7 @@ func plot_points() -> void:
 func vec_to_canvas_pos(vec: Vector2) -> Vector2:
 	var rect := get_rect()
 	var out := Vector2.ZERO
-	var tx = vec.x / max(domain_max, 0.5)
+	var tx = vec.x / domain
 	var ty = 1 - inverse_lerp(value_min, value_max, vec.y)
 	out.x = tx * (rect.size.x - PADDING * 2) + PADDING
 	out.y = ty * (rect.size.y - PADDING * 2) + PADDING
